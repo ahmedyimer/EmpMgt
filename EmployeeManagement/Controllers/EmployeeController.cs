3941c47b3a858a11b88db40a20802f05f4bef5da
Ahmed Yimer <ayimer@hfflp.com>
2019-11-08 14:30:17 -0600

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeManagement.Models;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //Singlton instances....
+        private readonly ILogger<EmployeeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
+
+        //inject to the constroctor. 
+        public EmployeeController(ILogger<EmployeeController> logger, IEmployeeRepository employeeRepository)
+        {
+            _logger = logger;
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            return View(employee);
+        }
+    }
+}
---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(ILogger<HomeController> logger, IEmployeeRepository employeeRepository)
+        {
+            _logger = logger;
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_employeeRepository.GetEmployee(1));
+        }
+
+        //public IActionResult Details()
+        //{
+        //    Employee model = _employeeRepository.GetEmployee(1);
+        //    return View(model);
+        //}
+
+        //public JsonResult Details()
+        //{
+        //    Employee model = _employeeRepository.GetEmployee(1);
+        //    return Json(model);
+        //}
+
+        //public ObjectResult Details()
+        //{
+        //    Employee model = _employeeRepository.GetEmployee(1);
+        //    return new ObjectResult(model);
+        //}
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+
+            // Pass PageTitle and Employee model to the View using ViewData
+            ViewData["PageTitle"] = "Employee Details";
+            ViewData["Employee"] = model;
+
+            return View(model);
+
+            //return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
